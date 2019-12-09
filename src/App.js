8888cad9ec2bbd6e4533975c09df84b03b4660a0
kumkumsingh <kumkumsingh.r@gmail.com>
2019-12-09 22:02:39 +0100

    Added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
+  state = {
+    joke :{},
+    punchLineText :'',
+    punchLine :""
+  }
+  componentDidMount(){
+    //fetching random jokes from an api using fetch request
+    fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(res => res.json())
+    .then(joke => {
+      //storing joke in an array jokes
+      this.setState({
+        joke : joke
+      })
+      console.log('checking data',joke)
+    })
+    //setting timelimit to show the punchline after 5 seconds.
+    setTimeout(() => {
+    this.setState({
+      punchLineText: 'PunchLine:',
+      punchLine :this.state.joke['punchline']
+    })
+    }, 5000);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  render(){
+    return (
+      <div className = "App-header">
+      <div>{`Programming Jokes {:`}</div>
+      <div>SetUp:{this.state.joke['setup']}</div>
+      <div className = "App">{this.state.punchLineText } {this.state.punchLine}</div>
+      <button className = "button">AddProgrammingJokes</button>
+      </div>
+    )
+  }
 }
 
 export default App;
